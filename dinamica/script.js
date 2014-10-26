e3cabe9ab33df72cd8ee26d6e7e7929f146c5e4d
leticia <leticiamaiat@gmail.com>
2014-10-26 17:45:55 -0300

    funciomalidade dos botoes adicionadas

--- dinamica/script.js
+++ dinamica/script.js
@@ -1,37 +1,17 @@
-$(".alert").alert();
 $(document).ready(function(){
-    $("#enviar").click(function(){
-    	if($("#input").val() == "" ){
-    		alert("Você deve inserir um texto.");
-    	} else if ($("#radioLearn").is(':checked')) {
-        	$("#learn").append("<li class=\"list-group-item ng-binding ng-scope\">"+ $("#input").val() +
-        	"<span class=\"pull-right\">" +
-        	"<button type=\"button\" class=\"btn btn-xs btn-success\"><span class=\"glyphicon glyphicon-arrow-right\"></span></button>"
-             + "<button class=\"btn btn-xs btn-info\"> <span class=\"glyphicon glyphicon-trash\"></span></button>" +
-        	"</span></li>");
-        } else {
-        	$("#learned").append("<li class=\"list-group-item ng-binding ng-scope\">"+ $("#input").val() +
-        	"<span class=\"pull-right\">" +
-        	"<button class=\"btn btn-xs btn-info\"> <span class=\"glyphicon glyphicon-trash\"></span></button>" +
-        	"</span></li>");
-        }
-    });
+  $("#cadastrar").click(function(){
+    var text = $("#disciplina").val();
+    $("#aprender").append("<li>"+text+"<button id=\"aprenda\">Aprender</button</li>");
+  });
+  $("#aprender").on("click", "#aprenda", function() {
+    var parent = $(this).parent();
+    var text = parent.text();
+    parent.remove();
+    $("#aprendidos").append("<li>"+text+"<button id=\"remove\">Remover</button</li>");
+  });
+  $("#aprendidos").on("click", "#remove", function() {
+    $(this).parent().remove();
+  });
 
-    $("#learn").on("click", ".btn-success", function() {
-       var parent = $(this).parent().parent();
-       var text = parent.text();
-        //parent.fadeOut();
-        parent.remove();
-        $("#learned").append("<li class=\"list-group-item ng-binding ng-scope\">"+ text+
-        	"<span class=\"pull-right\">" +
-        	"<button class=\"btn btn-xs btn-info\"> <span class=\"glyphicon glyphicon-trash\"></span></button>" +
-        	"</span></li>");
-    });
-
-    $(".list-group").on("click", ".btn-info", function() {
-       var parent = $(this).parent().parent();
-        //parent.fadeOut();
-        parent.remove();
-    });
 
 });
