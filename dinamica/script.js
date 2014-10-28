44e72bda3abac2f80c55b194a01ae4a3dbdb84fb
lucasmf <ldmatos8@gmail.com>
2014-10-28 17:52:48 -0300

    modifando design

--- dinamica/script.js
+++ dinamica/script.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function()
   $("#cadastrar").click(function(){
     var text = $("#disciplina").val();
     $("#aprender").append("<li class=\"list-group-item\">"
       +text
+      +"<span class=\"pull-right\">"
       +"<button id =\"addBtn\" type=\"button\" class=\"btn btn-default btn-lg btn-xs btn-success\"><span class=\"glyphicon glyphicon-ok\"></span></button>"
-      +"<button id =\"removeBtn\" type=\"button\" class=\"btn btn-default btn-lg btn-xs btn-danger\"><span class=\"glyphicon glyphicon-remove\"></span></button>");
+      +"<button id =\"removeBtn\" type=\"button\" class=\"btn btn-default btn-lg btn-xs btn-danger\"><span class=\"glyphicon glyphicon-remove\"></span></button>"
+	  +"</span>"
+	  +"</li>");
   });
   $("#aprender").on("click", "#addBtn", function() {
-    var parent = $(this).parent();
+    var parent = $(this).parent().parent();
     var text = parent.text();
     parent.remove();
     $("#aprendidos").append("<li class=\"list-group-item\">"
       +text
-      +"<button id =\"removeBtn\" type=\"button\" class=\"btn btn-default btn-lg btn-xs btn-danger\"><span class=\"glyphicon glyphicon-remove\"></span></button>");
+      + "<span class=\"pull-right\">"
+      +"<button id =\"removeBtn\" type=\"button\" class=\"btn btn-default btn-lg btn-xs btn-danger\"><span class=\"glyphicon glyphicon-remove\"></span></button>"
+	  +"</span>"
+	  +"</li>");
   });
   $("#aprender").on("click", "#removeBtn", function() {
-    $(this).parent().remove();
+    $(this).parent().parent().remove();
   });
   $("#aprendidos").on("click", "#removeBtn", function() {
-    $(this).parent().remove();
+    $(this).parent().parent().remove();
   });
 });
